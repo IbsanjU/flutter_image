dc0c3b564ebcf60f9bff235f376f0f28dee3393b
IbsanjU <bharath2668@gmail.com>
2021-07-06 11:48:01 +0530

    video picker and play

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/basic.dart';
-import 'package:flutter/src/widgets/container.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,7 +33,8 @@ class _MyHomePageState extends State<MyHomePage> {
   PickedFile _imageFile;
   dynamic _pickImageError;
   bool isVideo = false;
-
+  VideoPlayerController _controller;
+  VideoPlayerController _toBeDisposed;
   String _retrieveDataError;
 
   final ImagePicker _picker = ImagePicker();
@@ -44,11 +44,30 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<void> _playVideo(PickedFile file) async {
     if (file != null && mounted) {
+      await _disposeVideoController();
+      if (kIsWeb) {
+        _controller = VideoPlayerController.network(file.path);
+        // In web, most browsers won't honor a programmatic call to .play
+        // if the video has a sound track (and is not muted).
+        // Mute the video so it auto-plays in web!
+        // This is not needed if the call to .play is the result of user
+        // interaction (clicking on a "play" button, for example).
+        await _controller.setVolume(0.0);
+      } else {
+        _controller = VideoPlayerController.file(File(file.path));
+        await _controller.setVolume(1.0);
+      }
+      await _controller.initialize();
+      await _controller.setLooping(true);
+      await _controller.play();
       setState(() {});
     }
   }
 
   void _onImageButtonPressed(ImageSource source, {BuildContext context}) async {
+    if (_controller != null) {
+      await _controller.setVolume(0.0);
+    }
     if (isVideo) {
       final PickedFile file = await _picker.getVideo(
           source: source, maxDuration: const Duration(seconds: 10));
@@ -77,15 +96,45 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   void deactivate() {
+    if (_controller != null) {
+      _controller.setVolume(0.0);
+      _controller.pause();
+    }
     super.deactivate();
   }
 
   @override
   void dispose() {
+    _disposeVideoController();
     maxWidthController.dispose();
     maxHeightController.dispose();
     qualityController.dispose();
     super.dispose();
+  }
+
+  Future<void> _disposeVideoController() async {
+    if (_toBeDisposed != null) {
+      await _toBeDisposed.dispose();
+    }
+    _toBeDisposed = _controller;
+    _controller = null;
+  }
+
+  Widget _previewVideo() {
+    final Text retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_controller == null) {
+      return const Text(
+        'You have not yet picked a video',
+        textAlign: TextAlign.center,
+      );
+    }
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: AspectRatioVideo(_controller),
+    );
   }
 
   Widget _previewImage() {
@@ -95,6 +144,8 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     if (_imageFile != null) {
       if (kIsWeb) {
+        // Why network?
+        // See https://pub.dev/packages/image_picker#getting-ready-for-the-web-platform
         return Image.network(_imageFile.path);
       } else {
         return Semantics(
@@ -153,7 +204,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         textAlign: TextAlign.center,
                       );
                     case ConnectionState.done:
-                      return _previewImage();
+                      return isVideo ? _previewVideo() : _previewImage();
                     default:
                       if (snapshot.hasError) {
                         return Text(
@@ -169,7 +220,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   }
                 },
               )
-            : (_previewImage()),
+            : (isVideo ? _previewVideo() : _previewImage()),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -196,6 +247,32 @@ class _MyHomePageState extends State<MyHomePage> {
               heroTag: 'image1',
               tooltip: 'Take a Photo',
               child: const Icon(Icons.camera_alt),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              onPressed: () {
+                isVideo = true;
+                _onImageButtonPressed(ImageSource.gallery);
+              },
+              heroTag: 'video0',
+              tooltip: 'Pick Video from gallery',
+              child: const Icon(Icons.video_library),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FloatingActionButton(
+              backgroundColor: Colors.red,
+              onPressed: () {
+                isVideo = true;
+                _onImageButtonPressed(ImageSource.camera);
+              },
+              heroTag: 'video1',
+              tooltip: 'Take a Video',
+              child: const Icon(Icons.videocam),
             ),
           ),
         ],
@@ -272,11 +349,52 @@ class _MyHomePageState extends State<MyHomePage> {
 typedef void OnPickImageCallback(
     double maxWidth, double maxHeight, int quality);
 
-@override
-void initState() {}
+class AspectRatioVideo extends StatefulWidget {
+  AspectRatioVideo(this.controller);
 
-@override
-void dispose() {}
+  final VideoPlayerController controller;
 
-@override
-Widget build(BuildContext context) {}
+  @override
+  AspectRatioVideoState createState() => AspectRatioVideoState();
+}
+
+class AspectRatioVideoState extends State<AspectRatioVideo> {
+  VideoPlayerController get controller => widget.controller;
+  bool initialized = false;
+
+  void _onVideoControllerUpdate() {
+    if (!mounted) {
+      return;
+    }
+    if (initialized != controller.value.initialized) {
+      initialized = controller.value.initialized;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(_onVideoControllerUpdate);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_onVideoControllerUpdate);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (initialized) {
+      return Center(
+        child: AspectRatio(
+          aspectRatio: controller.value?.aspectRatio,
+          child: VideoPlayer(controller),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+}
